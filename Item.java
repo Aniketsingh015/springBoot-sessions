960e0385f029623923e52da6bca3cca74648c563
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-08 15:44:32 +0530

    Some changes

---
+++ Item.java
@@ -0,0 +1,30 @@
+public class Item {
+    private int itemNo;
+    private String itemName;
+    private double itemPrice;
+
+    // Gtters and setter
+
+    public int getItemNo() {
+        return itemNo;
+    }
+
+    public void setItemNo(){
+        this.itemNo=itemNo;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+    public void setItemName(){
+        this.itemName=itemName;
+    }
+
+    public double getItemPrice(){
+        return itemPrice;
+    }
+
+    public void setItemPrice(){
+        this.itemPrice=itemPrice;
+    }
+}
