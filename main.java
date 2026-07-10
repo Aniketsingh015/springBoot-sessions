c140b9a2046bff45c2e4b6cf582192a74de617a1
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-10 21:15:53 +0530

    Changes

--- main.java
+++ main.java
@@ -4,7 +4,16 @@ public class main {
         company.setName("Tech Innovators");
         System.out.println("Company Name: " + company.getName());
 
-        
+        // company decided to add some items to its catalog
+        var laptop = new Item(101, "Laptop", 1200.00);
+        var smartPhone = new Item(102, "Smartphone", 800.00);
+        company.addItem(laptop);
+        company.addItem(smartPhone);
+
+        System.out.println("Items in Company Catalog:");
+        for(Item item:company.getItems()){
+            System.out.println(item.getItemName()+ "-INR" +item.getItemPrice() + " Item No: " + item.getItemNo());
+        }
 
 
        
